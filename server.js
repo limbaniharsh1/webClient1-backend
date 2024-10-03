4bd7c6067b8add53f5299e10c236574464efda7d
harsh limbani <limbaniharsh1@gmail.com>
2024-10-03 22:14:24 +0530

    add cors from anywhere

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
-const contactRoutes = require("./features/contectUs/routes.js")
-const connectDB  = require("./config/db.js");
+const contactRoutes = require("./features/contectUs/routes.js");
+const connectDB = require("./config/db.js");
+const cors = require(cors)
 
 dotenv.config();
 const app = express();
+
+app.use(cors());
 
 // Middleware
 app.use(bodyParser.json());
@@ -19,5 +22,6 @@ connectDB();
 // Start server
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
+  // https://webclient1-backend.onrender.com
   console.log(`Server running on port ${PORT}`);
 });
